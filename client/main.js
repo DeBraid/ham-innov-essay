cab4456854c5a66b4e4cf7e8e41426927a89da8c
DeBraid <de.braid@gmail.com>
2015-03-24 07:48:50 -0400

    talent section edit

--- client/main.js
+++ client/main.js
@@ -1,31 +1,20 @@
-/* ---------------------------------------------------- +/
-
-## Main ##
-
-/+ ---------------------------------------------------- */
-
 Meteor.methods({
   goToByScroll: function ( id ) {
-    
     $('html,body').animate({
         scrollTop: $(id).offset().top
       },'slow');
-
   }
 });
 
 Template.homepage.events({
   'click #leftCol a': function ( e , t ) {
     e.preventDefault();     
-    
     var targetDiv = $(e.target).attr('href'),
         offset = 80;
-
     window.location.hash = targetDiv;
 
-    $($(e.target).attr('href'))[0].scrollIntoView();
+    // $($(e.target).attr('href'))[0].scrollIntoView();
     scrollBy(500, offset);
-
     Meteor.call('goToByScroll', targetDiv);
   }
 });
