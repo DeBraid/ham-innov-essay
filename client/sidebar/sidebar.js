c3c587ad0713ee547cfda7c726669e78b24355f9
DeBraid <de.braid@gmail.com>
2015-08-01 16:01:39 -0400

    move navbar into own folder, css clean up

--- client/sidebar/sidebar.js
+++ client/sidebar/sidebar.js
@@ -2,52 +2,40 @@ Template.sidebar.helpers({
 	sections: function () {
 		return [{
 		    id: 'summary',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Summary'
 		}, {
 		    id: 'intro',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Introduction'
 		}, {
 		    id: 'recipe',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Startup Recipe'
 		}, {
 		    id: 'innovation',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Innovation'
 		}, {
 		    id: 'capitalInCanada',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Capital In Canada'
 		}, {
 		    id: 'software',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Software'
 		}, {
 		    id: 'startups',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Starting Startups'
 		}, {
 		    id: 'management',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Management'
 		}, {
 		    id: 'risk',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Risk'
 		}, {
 		    id: 'talent',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Talent'
 		}, {
 		    id: 'conclusion',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Conclusion'
 		}, {
 		    id: 'charts',
-		    icon: 'fa fa-pencil'
 		    pretty: 'Charts'
-		}];
+		}]
 	}
 });
